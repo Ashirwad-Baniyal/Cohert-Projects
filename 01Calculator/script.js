2ae92eefe7904f75387d07b3422f29747ef02537
Ashirwad Baniyal <ashirwadbaniyal789@gmail.com>
2025-02-17 22:00:15 +0530

    Basic Calculator

---
+++ 01Calculator/script.js
@@ -0,0 +1,19 @@
+        function clearScreen() {
+            document.getElementById("screen").value = "";
+        }
+        function appendToScreen(value) {
+            document.getElementById("screen").value += value;
+        }
+
+        function calculate() {
+            let expression = document.getElementById("screen").value;
+            if (expression.includes('%')) {
+                expression = expression.replace(/%/g, '/100');  
+            }
+            try {
+                let result = eval(expression);
+                document.getElementById("screen").value = result;
+            } catch (error) {
+                document.getElementById("screen").value = "Error";
+            }
+        }
